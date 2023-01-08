4747e9160f37da76b1b64a5bfc95d7b4bc2b102b
virus <virusbb001a@gmail.com>
2023-01-08 16:19:14 +0900

    implements increment button

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,13 +4,24 @@ function main() {
     throw new Error("container not found");
   }
 
+  let countNumber = 0
+
   const displayCount = document.createElement("span");
-  displayCount.textContent = "0"
+  function updateDisplay () {
+    displayCount.textContent = countNumber.toString();
+  };
 
   const incrementButton = document.createElement("button");
   incrementButton.textContent = "Increment";
   const decrementButton = document.createElement("button");
   decrementButton.textContent = "Decrement";
+
+  incrementButton.addEventListener("click", () => {
+    countNumber = countNumber + 1;
+    updateDisplay();
+  });
+
+  updateDisplay();
 
   container.appendChild(displayCount);
   container.appendChild(incrementButton);
--- tests/e2e/main.spec.ts
+++ tests/e2e/main.spec.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { _electron as electron } from "playwright";
-import { ElectronApplication, test, expect } from "@playwright/test";
+import { ElectronApplication, test, expect, Page } from "@playwright/test";
 
 function launchApp (): Promise<ElectronApplication> {
   return electron.launch({
@@ -18,13 +18,28 @@ test.afterEach(async () => {
   await electronApp.close();
 });
 
+function generateLocator (window: Page) {
+  return {
+    displayCount: window.locator("#container > span"),
+    incButton: window.getByText("increment"),
+    decButton: window.getByText("Decrement")
+  }
+}
+
 test("show one number and two buttons increment and decrement", async () => {
   const window = await electronApp.firstWindow();
-  const displayCount = window.locator("#container > span");
-  const incButton = window.getByText("increment");
-  const decButton = window.getByText("Decrement");
+  const { displayCount, incButton, decButton } = generateLocator(window);
 
   await expect(displayCount).toBeVisible();
   await expect(incButton).toBeVisible();
   await expect(decButton).toBeVisible();
+});
+
+test("increment number when click inc button", async () => {
+  const window = await electronApp.firstWindow();
+  const { displayCount, incButton } = generateLocator(window);
+
+  await expect(displayCount).toHaveText("0");
+  await incButton.click()
+  await expect(displayCount).toHaveText("1");
 });
