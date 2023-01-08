4d34e4c6c87c412b9119eee7c47a5bd049c60311
virus <virusbb001a@gmail.com>
2023-01-08 14:45:34 +0900

    test: check visible "Hello World."

--- tests/e2e/main.spec.ts
+++ tests/e2e/main.spec.ts
@@ -1,10 +1,24 @@
 import path from "path";
 import { _electron as electron } from "playwright";
-import { test } from "@playwright/test";
+import { ElectronApplication, test, expect } from "@playwright/test";
 
-test("launch app", async () => {
-  const electronApp = await electron.launch({
+function launchApp (): Promise<ElectronApplication> {
+  return electron.launch({
     args: [path.join(__dirname, "..", "..", "dist", "main.js")]
   })
+}
+
+let electronApp: ElectronApplication
+
+test.beforeEach(async () => {
+  electronApp = await launchApp();
+});
+
+test.afterEach(async () => {
   await electronApp.close();
+});
+
+test("contains page that contains hello world", async () => {
+  const window = await electronApp.firstWindow();
+  await expect(window.getByText("Hello World.")).toBeVisible()
 });
